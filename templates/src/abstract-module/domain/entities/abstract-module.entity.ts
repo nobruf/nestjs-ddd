27d69291bd58b584943db72e96d535db57c94cea
Bruno Felipy <brunofelipems@outlook.com>
2024-08-09 20:00:45 -0300

    1.0.3

--- templates/src/abstract-module/domain/entities/abstract-module.entity.ts
+++ templates/src/abstract-module/domain/entities/abstract-module.entity.ts
@@ -1,28 +1,28 @@
 import { Entity } from '@/shared/domain/entities/entity';
 import { EntityValidationError } from '@/shared/domain/errors/validation-error';
-import { {{ModuleNameMin}}ValidatorFactory } from '../validators/{{ModuleNameMin}}.validator';
+import { {{ModuleName}}ValidatorFactory } from '../validators/{{ModuleNameMin}}.validator';
 
-export type {{ModuleNameMin}}Props = {
+export type {{ModuleName}}Props = {
   createdAt?: Date;
 };
 
-export class {{ModuleNameMin}}Entity extends Entity<{{ModuleNameMin}}Props> {
+export class {{ModuleName}}Entity extends Entity<{{ModuleName}}Props> {
   constructor(
-     public readonly props: {{ModuleNameMin}}Props,
+     public readonly props: {{ModuleName}}Props,
      id?: string,
     ) {
-     {{ModuleNameMin}}Entity.validate(props);
+     {{ModuleName}}Entity.validate(props);
      super(props, id);
      this.props.createdAt = this.props.createdAt ?? new Date();
   }
 
-  update(props: Partial<{{ModuleNameMin}}Props>) {
-    {{ModuleNameMin}}Entity.validate({ ...this.props, ...props });
+  update(props: Partial<{{ModuleName}}Props>) {
+    {{ModuleName}}Entity.validate({ ...this.props, ...props });
     Object.assign(this.props, props);
   }
 
-  static validate(props: {{ModuleNameMin}}Props) {
-    const validator = {{ModuleNameMin}}ValidatorFactory.create();
+  static validate(props: {{ModuleName}}Props) {
+    const validator = {{ModuleName}}ValidatorFactory.create();
     const isValid = validator.validate(props);
     if (!isValid) {
       throw new EntityValidationError(validator.errors);
